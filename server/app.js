f31ce86435285daedbb8c87c5f8258566470308a
Muhammad Salif <muhammad.salif@yahoo.com>
2020-11-21 22:42:38 +0500

    Minor fixed.

--- server/app.js
+++ server/app.js
@@ -4,17 +4,9 @@ const socketIo = require('socket.io');
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    next();
-});
 //implementing CORS
 //Access-control-Allow-Origin(Allowing Everyone to use our API)
 app.use(cors());
-app.options('*', cors());
 const index = require('./routes/index');
 app.use(index);
 
@@ -25,19 +17,18 @@ const server = app.listen(port, () => {
 
 const io = socketIo(server, {
     cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
+        origin: "*"
     },
 });
 
 let interval;
 
 io.on('connection', (socket) => {
-    console.log('New client connected');
     if (interval) {
         clearInterval(interval);
     }
     interval = setInterval(() => getApiAndEmit(socket), 1000);
+    console.log('New client connected');
     socket.on('disconnect', () => {
         console.log('Client disconnected');
         clearInterval(interval);
@@ -46,6 +37,8 @@ io.on('connection', (socket) => {
 
 const getApiAndEmit = (socket) => {
     const response = new Date();
+    
     // Emitting a new message. Will be consumed by the client
+    
     socket.emit('FromAPI', response);
 };
